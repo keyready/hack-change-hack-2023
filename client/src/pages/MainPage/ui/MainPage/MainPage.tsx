a83faedadd0e80352c93aa8aa49fb748f97322a7
keyready <vip.korchak@list.ru>
2023-11-19 11:12:41 +0300

    сделал чат, хых

--- client/src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -1,12 +1,15 @@
 import { Page } from 'widgets/Page/Page';
-import { useCallback, useEffect, useState } from 'react';
+import { FormEvent, useCallback, useEffect, useState } from 'react';
 import { Input } from 'shared/UI/Input';
 import { Button } from 'shared/UI/Button';
+import { VStack } from 'shared/UI/Stack';
+import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './MainPage.module.scss';
 
 interface Message {
-    sender: string;
-    body: string;
+    type: 'connection' | 'message';
+    username: string;
+    message: string;
 }
 
 const MainPage = () => {
@@ -14,38 +17,106 @@ const MainPage = () => {
         document.title = 'Hack&Change 2023';
     }, []);
 
-    const [socket, setSocket] = useState<WebSocket>(new WebSocket('ws://localhost:5000'));
     const [username, setUsername] = useState<string>('');
-    const [messages, setMessages] = useState<string[]>([]);
+    const [password, setPassword] = useState<string>('');
+    const [newMessage, setNewMessage] = useState<string>('');
+    const [isConnected, setIsConnected] = useState<boolean>(false);
+
+    const [socket, setSocket] = useState<WebSocket>(new WebSocket('ws://localhost:5000'));
+    const [messages, setMessages] = useState<Message[]>([]);
 
     useEffect(() => {
         socket.onopen = () => {
-            console.log('Подлкючение установлено');
-            socket.send('привет сервер');
+            console.log('Подключение установлено');
         };
 
         socket.onmessage = (event) => {
-            setMessages((prev) => [...prev, event.data]);
-        };
-    }, [messages, socket]);
+            const data = JSON.parse(event.data);
 
-    const sendMessage = useCallback(() => {
-        socket.send(username);
-    }, [socket, username]);
+            switch (data.type) {
+                case 'connection':
+                    if (data.username === username) {
+                        setIsConnected(true);
+                    }
+                    setMessages((prevState) => [data, ...prevState]);
+                    break;
+
+                case 'message':
+                    setMessages((prev) => [data, ...prev]);
+                    break;
+
+                case 'disconnect':
+                    setMessages((prevState) => [data, ...prevState]);
+                    break;
+            }
+        };
+    }, [messages, socket, username]);
+
+    const sendMessage = useCallback(
+        (event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+
+            socket.send(
+                JSON.stringify({
+                    type: 'message',
+                    body: newMessage,
+                    username,
+                }),
+            );
+            setNewMessage('');
+        },
+        [newMessage, socket, username],
+    );
+
+    const makeConnection = useCallback(
+        (event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+
+            socket.send(
+                JSON.stringify({
+                    type: 'connection',
+                    username,
+                    password,
+                }),
+            );
+        },
+        [password, socket, username],
+    );
+
+    if (!isConnected) {
+        return (
+            <VStack maxW>
+                <form onSubmit={makeConnection}>
+                    <Input value={username} onChange={setUsername} />
+                    <Input value={password} onChange={setPassword} />
+                    <Button type="submit">Войти</Button>
+                </form>
+            </VStack>
+        );
+    }
 
     return (
         <Page>
-            <Input value={username} onChange={setUsername} />
-            <Button onClick={sendMessage}>Send</Button>
+            <form onSubmit={sendMessage}>
+                <Input value={newMessage} onChange={setNewMessage} />
+                <Button type="submit">Отправить</Button>
+            </form>
 
             <hr style={{ width: '100%', height: '2px', margin: '15px 0px' }} />
 
-            {messages.length &&
-                messages.map((message, index) => (
-                    <div key={index} className={classes.messageCard}>
-                        {message}
-                    </div>
-                ))}
+            <VStack maxW gap="16">
+                {messages.length &&
+                    messages.map((message, index) => (
+                        <div
+                            key={index}
+                            className={classNames(classes.messageCard, {
+                                [classes.ownMessage]: message.username === username,
+                            })}
+                        >
+                            {message.username}@ {message.message}
+                        </div>
+                    ))}
+            </VStack>
         </Page>
     );
 };
--- server/server.js
+++ server/server.js
@@ -3,16 +3,52 @@ const express = require('express');
 const app = express();
 const ws = require('express-ws')(app);
 
-app.ws('/', (ws, req) => {
-    console.log('WebSocket connection opened');
+const aWss = ws.getWss();
 
+const broadcastMessaging = (message) => {
+    aWss.clients.forEach((client) => {
+        client.send(message);
+    });
+};
+
+app.ws('/', (ws, req) => {
     ws.on('message', (event) => {
-        console.log(event);
-        ws.send(event);
+        const message = JSON.parse(event);
+
+        console.log(message);
+
+        switch (message.type) {
+            case 'connection':
+                broadcastMessaging(
+                    JSON.stringify({
+                        username: message.username,
+                        type: 'connection',
+                        message: `Пользователь ${message.username} подключился`,
+                    }),
+                );
+                break;
+
+            case 'message':
+                broadcastMessaging(
+                    JSON.stringify({
+                        username: message.username,
+                        message: message.body,
+                        type: 'message',
+                    }),
+                );
+                break;
+        }
     });
 
     ws.on('close', () => {
         console.log('WebSocket connection closed');
+        broadcastMessaging(
+            JSON.stringify({
+                type: 'disconnect',
+                username: 'message.username',
+                message: `Пользователь вышел из чата`,
+            }),
+        );
     });
 });
 
